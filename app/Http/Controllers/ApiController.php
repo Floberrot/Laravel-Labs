d4d819d7ff44fd913c43ace71104907a86977151
floberrot <florian.berrot92@gmail.com>
2025-08-11 11:23:47 +0200

    :zap: add validator rules

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use function Symfony\Component\Translation\t;
 
 class ApiController extends Controller
 {
     public function store(Request $request): void
     {
+        $request->validate([
+            'test' => 'required|string'
+        ]);
+
         $json = $request->json();
 
         echo $json->get('test');
