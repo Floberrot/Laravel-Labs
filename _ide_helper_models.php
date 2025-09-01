5a3b1a152bff6a12dd9a33251b0351e99c1b7472
floberrot <florian.berrot92@gmail.com>
2025-09-01 10:50:00 +0200

    :wrench: conf files

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -11,3 +11,179 @@
  */
 
 
+namespace App\Models{
+/**
+ * @property int $id
+ * @property mixed|null $title
+ * @property string $author
+ * @property \Illuminate\Support\Carbon $published_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int $available
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Comment> $comments
+ * @property-read int|null $comments_count
+ * @property-read \App\Models\BookDetail|null $detail
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Tag> $tags
+ * @property-read int|null $tags_count
+ * @method static \Database\Factories\BookFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Book newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Book newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Book query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Book whereAuthor($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Book whereAvailable($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Book whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Book whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Book wherePublishedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Book whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Book whereUpdatedAt($value)
+ */
+	class Book extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * @property int $id
+ * @property string $isbn
+ * @property int|null $pages
+ * @property int $book_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \App\ValueObject\Price\Price|null $price
+ * @property-read \App\Models\Book $book
+ * @property-read mixed $pages_human
+ * @method static \Database\Factories\BookDetailFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BookDetail newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BookDetail newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BookDetail query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BookDetail whereBookId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BookDetail whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BookDetail whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BookDetail whereIsbn($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BookDetail wherePages($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BookDetail wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BookDetail whereUpdatedAt($value)
+ */
+	class BookDetail extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BookTag newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BookTag newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|BookTag query()
+ */
+	class BookTag extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * @property int $id
+ * @property string $content
+ * @property bool $approved
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int $book_id
+ * @property-read \App\Models\Book $book
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereApproved($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereBookId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereUpdatedAt($value)
+ */
+	class Comment extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * @property int $id
+ * @property string $message
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \App\Enums\LogTypeEnum $type
+ * @property-read mixed $info_color
+ * @method static \Database\Factories\LogFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Log newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Log newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Log query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Log whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Log whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Log whereMessage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Log whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Log whereUpdatedAt($value)
+ */
+	class Log extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * @property int $id
+ * @property string $title
+ * @property string|null $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Post newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Post newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Post query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Post whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Post whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Post whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Post whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Post whereUpdatedAt($value)
+ */
+	class Post extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * @property int $id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Book> $books
+ * @property-read int|null $books_count
+ * @method static \Database\Factories\TagFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tag newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tag newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tag query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tag whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tag whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tag whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tag whereUpdatedAt($value)
+ */
+	class Tag extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
